5b6d9b6e937d47b1efda49140a82618c3cd7182e
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2024-02-26 23:07:26 -0600

    Update startup.ps1
    Modified startup files to check if the flask env exists, if it does activate and install requirements. If it doesn't create the venv and then activate and install

--- startup.ps1
+++ startup.ps1
@@ -1,12 +1,14 @@
-# create env folder
-mkdir env
-# change directory to env
-cd env
-# create virtual environment
-python -m venv flask_env
-# go back to the root directory
-cd ..
-# activate the virtual environment
-env\flask_env\Scripts\activate
-# install requirements.txt
+IF NOT EXIST env\flask_env (
+    :: create env folder
+    mkdir env
+    :: change directory to env
+    cd env
+    :: create virtual environment
+    python -m venv flask_env
+    :: go back to the root directory
+    cd ..
+)
+:: activate the virtual environment
+call env\flask_env\Scripts\activate
+:: install requirements.txt
 pip install -r requirements.txt
