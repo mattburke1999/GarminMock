38c2eb50dcea27ef506a0b0524044941f607db86
Matthew Burke <matthew.burke@givex.com>
2024-02-26 23:16:30 -0600

    Updated startup files to check if venv was created before creating one, so I can run just the activate and install when needed

--- startup.ps1
+++ startup.ps1
@@ -1,14 +1,16 @@
-IF NOT EXIST env\flask_env (
-    :: create env folder
+# Check if 'env\flask_env' directory exists
+if (-not (Test-Path -Path env\flask_env)) {
+    # create env folder
     mkdir env
-    :: change directory to env
-    cd env
-    :: create virtual environment
+    # change directory to env
+    Set-Location env
+    # create virtual environment
     python -m venv flask_env
-    :: go back to the root directory
-    cd ..
-)
-:: activate the virtual environment
-call env\flask_env\Scripts\activate
-:: install requirements.txt
+    # go back to the root directory
+    Set-Location ..
+}
+
+# activate the virtual environment
+. .\env\flask_env\Scripts\activate
+# install requirements.txt
 pip install -r requirements.txt
